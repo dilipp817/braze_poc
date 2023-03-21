d93c19f7f49b0c7e8b3e84159f27c5ca0857d44e
Dilip Patel <dilip_patel@discovery.com>
2023-03-21 17:38:35 +0530

    logging purchase event

--- BrazeDemo/app/src/main/java/com/fnk/braze/MainActivity.kt
+++ BrazeDemo/app/src/main/java/com/fnk/braze/MainActivity.kt
@@ -2,10 +2,23 @@ package com.fnk.braze
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.braze.Braze
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logPurchase()
+    }
+
+    private fun logPurchase() {
+        Braze.getInstance(this).logPurchase(
+            productId= "",
+            currencyCode= "String",
+            price= BigDecimal("0.04"),
+            quantity= 8
+        )
     }
 }
